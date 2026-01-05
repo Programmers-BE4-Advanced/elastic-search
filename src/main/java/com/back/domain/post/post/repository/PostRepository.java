ed1ff751cba917c112ba74893eccb5ce69d8e704
NamSangwon <nsw3265@naver.com>
2026-01-05 16:32:53 +0900

    0012 - add comment

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import java.util.List;
 
 public interface PostRepository extends ElasticsearchRepository<Post, String> {
+    /*
+    💡 왜 findAll()을 재선언하나요?
+        - ElasticsearchRepository의 기본 findAll()은 Iterable<Post>를 반환합니다.
+        - List<Post> 반환 타입으로 재선언하면 Spring Data가 자동으로 List로 변환해줍니다.
+     */
     List<Post> findAll();
 }
